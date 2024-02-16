96b7344b32268e6b24b4eb9d7ae90b9dab20c980
makecolour <quyennguyen083004@gmail.com>
2024-02-16 18:45:22 +0700

    c

--- content/auto_edunext.js
+++ content/auto_edunext.js
@@ -1,4 +1,4 @@
-const models = ["gpt-4-vision-preview", "gpt-4", "gpt-3.5-turbo", "gpt-3.5-turbo-instruct"]
+const models = ["gpt-3.5-turbo", "gpt-4-vision-preview", "gpt-4",  "gpt-3.5-turbo-instruct"]
 const endPoints = ["https://api.openai.com/v1/chat/completions", "https://api.openai.com/v1/completions"]
 
 async function getTxt() {
@@ -38,32 +38,48 @@ async function getTxt() {
 
 async function promptChatGPT(prompt, api, model) {
   const modelName = model; // Specify the model name here
-  var url
+  var url, requestOptions;
   if (model == models[0] || model == models[1] || model == models[2]) {
     url = endPoints[0];
+    requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${api}`
+      },
+      body: JSON.stringify({
+        model: modelName,
+        messages: [
+          {
+            role: "user",
+            content: prompt
+          }
+        ],
+        max_tokens: 2000, // Maximum number of tokens (words) the model should return
+        temperature: 0.8, // Controls the randomness of the output
+        //stop: '\n', // Stops generation at a specific token
+      })
+    };
   }
   else {
-    url = endPoints[0];
-  }
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${api}`
-    },
-    body: JSON.stringify({
-      model: modelName,
-      messages: [
-        {
-          role: "user",
-          content: prompt
-        }
-      ],
-      max_tokens: 2000, // Maximum number of tokens (words) the model should return
-      temperature: 0.8, // Controls the randomness of the output
-      //stop: '\n', // Stops generation at a specific token
-    })
+    url = endPoints[1];
+    requestOptions = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+          model: modelName,
+          prompt: prompt,
+          max_tokens: 2000, // Adjust as needed
+          temperature: 0.8, // Adjust as needed
+          n: 1 // Number of completions to generate
+      })
   };
+  }
+  
+
   console.log(requestOptions)
   return fetch(url, requestOptions)
     .then(response => response.json())
